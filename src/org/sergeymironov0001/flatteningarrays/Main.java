51386cc410d0d4c789b828d5fc65437284b5cba6
Sergey_Mironov <sergeymironov0001@gmail.com>
2017-11-13 21:23:13 +0300

    Implement recursive flattening arrays

--- src/org/sergeymironov0001/flatteningarrays/Main.java
+++ src/org/sergeymironov0001/flatteningarrays/Main.java
@@ -1,8 +1,18 @@
 package org.sergeymironov0001.flatteningarrays;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Object[] array = createArray(1, 2, 3, createArray(4, 5));
+        System.out.println(Arrays.asList(FlatteningArrays.recursiveFlatten(array)));
+
+        Object[] array1 = createArray(1, createArray(2, createArray(3, createArray(4, 5))));
+        System.out.println(Arrays.asList(FlatteningArrays.recursiveFlatten(array1)));
+    }
+
+    private static Object[] createArray(Object... elements) {
+        return elements;
     }
 }
